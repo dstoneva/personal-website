2cd67f8c121588a06187c2f3f31b671be2290b33
dstoneva <stoilova.denitsa@gmail.com>
2023-02-24 18:36:19 +0200

    Create about view

---
+++ src/views/About/About.js
@@ -0,0 +1,38 @@
+import { Grid, Typography } from '@mui/material'
+import { OnScreen } from 'motions'
+import { Terminal } from 'components'
+import { personalInfo } from 'utils/constants'
+import { Hobbies } from './components'
+
+const About = () => {
+  return (
+        <Grid
+          container
+          sx={{
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            px: 4
+          }}
+          spacing={2}
+        >
+          <Grid item xs={12} md={10} lg={8} sx={{ mx: { md: 'auto', lg: 2 } }}>
+            <OnScreen>
+              <Terminal
+                title='about me'
+                directory='about-me'
+                content={
+                  <>
+                    <Typography align='left' variant='body' color={(theme) => theme.palette.success.main}>
+                      {personalInfo.aboutMe}
+                    </Typography>
+                    <Hobbies />
+                  </>
+                }
+              />
+            </OnScreen>
+          </Grid>
+        </Grid>
+  )
+}
+
+export default About
---
+++ src/views/About/components/Hobbies/Hobbies.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+import { personalInfo } from 'utils/constants'
+
+export default function Hobbies() {
+  return (
+    <Box sx={{ mt: 1 }}>
+      <Typography sx={{ mb: 1 }}>Apart from coding, my interests include:</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+        {personalInfo.hobbies.map((hobby) => (
+          <Box display='flex' alignItems='center' sx={{ gap: 1 }} key={hobby.label}>
+            <Box component='span' fontSize='1.25rem'>
+              {hobby.emoji}
+            </Box>
+            <Typography>{hobby.label}</Typography>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  )
+}
