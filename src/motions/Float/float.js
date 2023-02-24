5d569da4b234f1e0983a30ac511e64534a165cf2
dstoneva <stoilova.denitsa@gmail.com>
2023-02-24 18:08:47 +0200

    Create float animation

---
+++ src/motions/Float/float.js
@@ -0,0 +1,14 @@
+import { keyframes } from '@emotion/react'
+
+const float = keyframes` 
+  0% {
+    transform: translatey(0px);
+  }
+  50% {
+    transform: translatey(-20px);
+  }
+  100% {
+    transform: translatey(0px);
+  }
+`
+export default float
