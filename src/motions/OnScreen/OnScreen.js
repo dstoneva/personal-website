d2f8ad5bd3697d12e90731a7ee56a8f995c0742c
dstoneva <stoilova.denitsa@gmail.com>
2023-02-24 18:11:01 +0200

    Create on screen animation

---
+++ src/motions/OnScreen/OnScreen.js
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion'
+
+const OnScreen = ({ children }) => {
+  const cardVariants = {
+    offscreen: {
+      y: 100,
+      opacity: 0,
+    },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  }
+
+  return (
+    <motion.div initial={cardVariants.offscreen} animate={cardVariants.onscreen}>
+      {children}
+    </motion.div>
+  )
+}
+
+export default OnScreen
