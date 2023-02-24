556c34daaeb7eedb64c3b8b2d06697b973074505
dstoneva <stoilova.denitsa@gmail.com>
2023-02-24 18:04:49 +0200

    Create personal info

---
+++ src/utils/constants/personalInfo.js
@@ -0,0 +1,22 @@
+const personalInfo = {
+  fullName: 'Denitsa Toneva',
+  dynamicText: ['Front End Developer', 'Engineer', 'Quick Learner'],
+  aboutMe: `I'm a front end developer based in Sofia. As a driven and detail-oriented developer with several months of experience, I have developed a solid understanding of HTML, CSS, and JavaScript, and have been able to utilize my knowledge to build engaging user interfaces. I am committed to staying up-to-date with the latest industry trends and technologies and am confident in my ability to contribute to any development project.`,
+  hobbies: [
+    {
+      label: 'reading',
+      emoji: '📖',
+    },
+    {
+      label: 'sport',
+      emoji: '🏀',
+    },
+    {
+      label: 'movies',
+      emoji: '🎥',
+    },
+  ],
+  email: 'denitza.toneva@gmail.com',
+}
+
+export default personalInfo
