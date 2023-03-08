2fdb72fb49070dd27c4346691b11e2cc58080fdf
dstoneva <stoilova.denitsa@gmail.com>
2023-03-08 19:54:31 +0200

    Update images

--- src/utils/constants/projectsInfo.js
+++ src/utils/constants/projectsInfo.js
@@ -2,7 +2,7 @@ const projectsInfo = [
   {
     title: 'E-commerce',
     logo: 'images/ecommerce_logo.png',
-    media: 'images/ecommerce.png',
+    media: 'images/ecommerce_desktop.png',
     info: 'E-commerce is an e-commerce platform that allows users to purchase various items through multi-step checkout form. The application is built using React and several popular React libraries including Formik, Yup, Axios, Notistack and more. The user interface is designed using Material-UI, and the application is a progressive web app (PWA), providing a seamless and fast experience for users on both desktop and mobile devices.',
     technologies: ['MUI', 'ReactJS'],
     demoURL: 'https://e-commerce-dstoneva.netlify.app',
@@ -11,7 +11,7 @@ const projectsInfo = [
   {
     title: 'Airbnb offer page',
     logo: 'images/airbnb_logo.png',
-    media: 'images/airbnb-offer.png',
+    media: 'images/airbnb_desktop.png',
     info: 'Airbnb offer page clone made using only HTML and SCSS. A showcase of all the page and component styling skills I have acquired.',
     technologies: ['HTML', 'SCSS'],
     demoURL: '',
@@ -20,7 +20,7 @@ const projectsInfo = [
   {
     title: 'Personal website',
     logo: 'images/portfolio_logo.png',
-    media: 'images/portfolio.png',
+    media: 'images/portfolio_desktop.png',
     info: 'You are here! A personal website made using ReactJS. The user interface is inspired by macOS and designed using Material-UI.',
     technologies: ['MUI', 'ReactJS'],
     demoURL: '',
@@ -29,7 +29,7 @@ const projectsInfo = [
   {
     title: 'Hangman',
     logo: 'images/hangman-logo.png',
-    media: 'images/hangman.png',
+    media: 'images/hangman_desktop.png',
     info: "A game of Hangman with a twist and inspired by Wordle. Made using Reactstrap and ReactJS. A showcase of using local storage to save the game's progress even if you refresh or leave the page.",
     technologies: ['Reactstrap', 'ReactJS'],
     demoURL: 'https://react-hangman-app.vercel.app',
