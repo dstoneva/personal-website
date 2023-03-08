a7925f1d4f31173e02f4a098b27e26fb014316d4
dstoneva <stoilova.denitsa@gmail.com>
2023-03-08 12:37:22 +0200

    Change background image

--- src/theme/overrides/MuiCssBaseline.js
+++ src/theme/overrides/MuiCssBaseline.js
@@ -4,19 +4,19 @@ export default (theme) => ({
   MuiCssBaseline: {
     styleOverrides: {
       body: {
-        background: '#26004d',
+        background: '#663399',
         width: '100vw',
         height: '100%',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        backgroundImage: `radial-gradient(at 32% 66%, hsla(11, 89%, 51%, 1) 0, hsla(11, 89%, 51%, 0) 50%),
-radial-gradient(at 9% 68%, hsla(215, 86%, 68%, 1) 0, hsla(215, 86%, 68%, 0) 50%),
-radial-gradient(at 73% 88%, hsla(245, 87%, 64%, 1) 0, hsla(245, 87%, 64%, 0) 50%),
-radial-gradient(at 99% 40%, hsla(333, 92%, 54%, 1) 0, hsla(333, 92%, 54%, 0) 50%),
-radial-gradient(at 51% 59%, hsla(207, 92%, 64%, 1) 0, hsla(207, 92%, 64%, 0) 50%),
-radial-gradient(at 59% 2%, hsla(198, 92%, 63%, 1) 0, hsla(198, 92%, 63%, 0) 50%),
-radial-gradient(at 52% 16%, hsla(144, 89%, 53%, 1) 0, hsla(144, 89%, 53%, 0) 50%)`,
+        backgroundImage: `radial-gradient(at 96% 92%, hsla(290,97%,60%,1) 0px, transparent 50%),
+        radial-gradient(at 48% 98%, hsla(216,88%,74%,1) 0px, transparent 50%),
+        radial-gradient(at 76% 43%, hsla(42,82%,66%,1) 0px, transparent 50%),
+        radial-gradient(at 44% 89%, hsla(176,87%,76%,1) 0px, transparent 50%),
+        radial-gradient(at 88% 17%, hsla(198,86%,62%,1) 0px, transparent 50%),
+        radial-gradient(at 27% 41%, hsla(318,96%,63%,1) 0px, transparent 50%)
+        `,
       },
       html: {
         scrollBehavior: 'smooth',
