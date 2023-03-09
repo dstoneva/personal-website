d02c34c48c94260893f19994fd12f15e23e2cc97
dstoneva <stoilova.denitsa@gmail.com>
2023-03-09 18:42:50 +0200

    Replace big images with compressed versions

--- src/utils/constants/projectsInfo.js
+++ src/utils/constants/projectsInfo.js
@@ -2,7 +2,7 @@ const projectsInfo = [
   {
     title: 'E-commerce',
     logo: 'images/ecommerce_logo.png',
-    media: 'images/ecommerce_desktop.png',
+    media: 'images/ecommerce_desktop.webp',
     info: 'E-commerce is an e-commerce platform that allows users to purchase various items through multi-step checkout form. The application is built using React and several popular React libraries including Formik, Yup, Axios, Notistack and more. The user interface is designed using Material-UI, and the application is a progressive web app (PWA), providing a seamless and fast experience for users on both desktop and mobile devices.',
     technologies: ['MUI', 'ReactJS'],
     demoURL: 'https://e-commerce-dstoneva.netlify.app',
@@ -11,7 +11,7 @@ const projectsInfo = [
   {
     title: 'Airbnb offer page',
     logo: 'images/airbnb_logo.png',
-    media: 'images/airbnb_desktop.png',
+    media: 'images/airbnb_desktop.webp',
     info: 'Airbnb offer page clone.',
     technologies: ['HTML', 'SCSS'],
     demoURL: 'https://dstoneva-airbnb-clone.vercel.app',
@@ -20,16 +20,16 @@ const projectsInfo = [
   {
     title: 'Personal website',
     logo: 'images/portfolio_logo.png',
-    media: 'images/portfolio_desktop.png',
+    media: 'images/portfolio_desktop.webp',
     info: 'You are here! A personal website made using ReactJS. The user interface is inspired by macOS and designed using Material-UI.',
     technologies: ['MUI', 'ReactJS'],
-    demoURL: '',
+    demoURL: 'https://dstoneva.netlify.app/',
     sourceURL: 'https://github.com/dstoneva/personal-website',
   },
   {
     title: 'Hangman',
     logo: 'images/hangman-logo.png',
-    media: 'images/hangman_desktop.png',
+    media: 'images/hangman_desktop.webp',
     info: "A game of Hangman with a twist and inspired by Wordle. Made using Reactstrap and ReactJS. This is a showcase of using local storage to save the game's progress even if you refresh or leave the page.",
     technologies: ['Reactstrap', 'ReactJS'],
     demoURL: 'https://dstoneva-hangman.vercel.app',
