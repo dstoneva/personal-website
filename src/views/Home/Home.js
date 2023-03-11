1eaa4e59d8d24e3636de659e4d3dab96bdcc8d1d
dstoneva <stoilova.denitsa@gmail.com>
2023-03-11 14:04:48 +0200

    Remove unnecessary styles

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -26,7 +26,6 @@ const Home = () => {
               <Typography
                 textAlign="center"
                 variant="terminal1"
-                color="common.white"
                 sx={{
                   fontWeight: (theme) => theme.typography.fontWeightMedium,
                 }}
@@ -36,7 +35,6 @@ const Home = () => {
               <Typography
                 textAlign="center"
                 variant="terminal1"
-                color="common.white"
                 sx={{
                   fontWeight: (theme) => theme.typography.fontWeightMedium,
                 }}
