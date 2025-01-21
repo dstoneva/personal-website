c194d7aa14b4d29211263eb0b714b7accf65c5b9
Denitsa Toneva <stoilova.denitsa@gmail.com>
2025-01-21 23:59:49 +0200

    Refactor Skillset component to use an array of skills instead of a single skill

--- src/views/Skills/components/Skillset/Skillset.js
+++ src/views/Skills/components/Skillset/Skillset.js
@@ -1,29 +1,27 @@
-import { Box, Tooltip } from '@mui/material'
+import { Box, Tooltip} from '@mui/material'
 import { Fragment } from 'react'
 
-const Skillset = ({ skill }) => {
+const Skillset = ({ skills }) => {
   return (
-      <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', m: 1 }}>
-        {skill.map((item) => {
-          return (
-            <Fragment key={item.title}>
-              <Tooltip title={item.title}>
-                <Box
-                  alt={item.title}
-                  component='img'
-                  sx={{
-                    margin: 1,
-                    maxWidth: { xs: '1.5rem', sm: '3rem' },
-                    maxHeight: { xs: '1.5rem', sm: '3rem' },
-                    '&:hover': { transform: 'scale3d(1.3, 1.3, 1)', transition: 'all .4s ease-in-out' },
-                  }}
-                  src={item.icon}
-                />
-              </Tooltip>
-            </Fragment>
-          )
-        })}
-      </Box>
+    <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', m: 1 }}>
+      {skills.map((item) => (
+        <Fragment key={item.title}>
+          <Tooltip title={item.title}>
+              <Box
+                alt={item.title}
+                component="img"
+                sx={{
+                  margin: 1,
+                  maxWidth: { xs: '1.5rem', sm: '3rem' },
+                  maxHeight: { xs: '1.5rem', sm: '3rem' },
+                  '&:hover': { transform: 'scale3d(1.3, 1.3, 1)', transition: 'all .4s ease-in-out' },
+                }}
+                src={item.icon}
+              />
+          </Tooltip>
+        </Fragment>
+      ))}
+    </Box>
   )
 }
 
