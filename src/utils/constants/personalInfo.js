2bf19162b6ab14c8c7f32077d9bdc7c1257f2381
Denitsa Toneva <stoilova.denitsa@gmail.com>
2025-01-22 00:05:16 +0200

    Refactor personalInfo.js to remove dynamic text and update data

--- src/utils/constants/personalInfo.js
+++ src/utils/constants/personalInfo.js
@@ -1,7 +1,14 @@
 const personalInfo = {
   fullName: 'Denitsa Toneva',
-  dynamicText: ['Front End Developer', 'Engineer', 'Quick Learner'],
-  aboutMe: `I'm a front end developer based in Sofia. As a driven and detail-oriented developer with several months of experience, I have developed a solid understanding of HTML, CSS, and JavaScript, and have been able to utilize my knowledge to build engaging user interfaces. I am committed to staying up-to-date with the latest industry trends and technologies and am confident in my ability to contribute to any development project.`,
+  dynamicText: ['Full Stack Developer', 'Engineer', 'Quick Learner'],
+  aboutMe: (
+    <div>
+      I’m a Full Stack Developer based in Sofia with close to 3 years of hands-on experience. <br />
+      Currently, I’m working on an IoT platform that enhances street lighting infrastructure through real-time
+      monitoring and remote management.
+      <br />I enjoy learning new technologies and contributing to meaningful projects in collaborative environments.
+    </div>
+  ),
   hobbies: [
     {
       label: 'reading',
@@ -18,7 +25,7 @@ const personalInfo = {
   ],
   email: 'denitza.toneva@gmail.com',
   skills:
-    'My experience includes developing responsive websites using HTML, CSS and JavaScript. I have also implemented AJAX for dynamic content loading and built user interfaces with ReactJS and React frameworks.',
+    'My professional experience includes building and deploying full-stack applications with ReactJS, TypeScript, NodeJS, and PostgreSQL. I’ve designed user interfaces with React frameworks, integrated Keycloak for authentication, documented APIs with Swagger, and implemented CI/CD pipelines to streamline deployments',
 }
 
 export default personalInfo
